8d821f6ca647b1636f0a77740a0026854dd37998
Anton <antonymousfeedback@gmail.com>
2020-06-05 09:04:48 +0200

    Include postHandler inline

--- src/lib/packager.js
+++ src/lib/packager.js
@@ -30,84 +30,82 @@ class Packager extends Service {
     this.minio = new Minio.Client(opts)
     this.api = api
 
-    api.app.post('/packages', (req, res) => this.postHandler(req, res))
-  }
+    api.app.post('/packages', async (req, res) => {
+      req.setTimeout(config.http.requestTimeoutSeconds * 1000)
 
-  async postHandler (req, res) {
-    req.setTimeout(config.http.requestTimeoutSeconds * 1000)
-
-    let { rootId, uuid } = req.body
-    if (uuid && !rootId) {
-      rootId = `${constants.BASE_URI}maps/${uuid}`
-    }
-    let archivePath, outDir, mapResults
-    const requestConfig = {
-      headers: {
-        Authorization: req.headers.authorization
+      let { rootId, uuid } = req.body
+      if (uuid && !rootId) {
+        rootId = `${constants.BASE_URI}maps/${uuid}`
       }
-    }
-
-    try {
-      mapResults = await fetchMap(rootId, {
-        metadata: {},
-        maps: [],
-        files: [],
-        cells: [],
-        annotations: []
-      }, requestConfig)
-    }
-    catch (err) {
-      this.api.captureException(err)
-      return send(res, 500, `fetchMap: ${err.message}`)
-    }
-
-    try {
-      outDir = path.join(os.tmpdir(), ObjectUtil.uuid4())
-      await fsx.ensureDir(outDir)
-
-      let needsTemplateFetch = true
-
-      if (needsTemplateFetch) {
-        await this.minio.fGetObject(config.assets.packagesBucket, 'template.zip', path.join(os.tmpdir(), 'template.zip'))
+      let archivePath, outDir, mapResults
+      const requestConfig = {
+        headers: {
+          Authorization: req.headers.authorization
+        }
       }
-    }
-    catch (err) {
-      this.api.captureException(err)
-      return send(res, 500, `fetchTemplate: ${err.message}`)
-    }
 
-    try {
-      const templateEntries = await extractTemplate(path.join(os.tmpdir(), 'template.zip'), outDir)
+      try {
+        mapResults = await fetchMap(rootId, {
+          metadata: {},
+          maps: [],
+          files: [],
+          cells: [],
+          annotations: []
+        }, requestConfig)
+      }
+      catch (err) {
+        api.captureException(err)
+        return send(res, 500, `fetchMap: ${err.message}`)
+      }
 
-      archivePath = `${outDir}.zip`
-      const archive = new yazl.ZipFile()
-      await processTemplate(outDir, rootId, templateEntries, mapResults, archive)
-      await storeData(outDir, rootId, mapResults, archive, requestConfig)
-      archive.end()
+      try {
+        outDir = path.join(os.tmpdir(), ObjectUtil.uuid4())
+        await fsx.ensureDir(outDir)
 
-      await new Promise((resolve, reject) => {
-        archive.outputStream.pipe(fs.createWriteStream(archivePath))
-          .on('error', err => reject(err))
-          .on('close', () => resolve())
-      })
-    }
-    catch (err) {
-      this.api.captureException(err)
-      return send(res, 500, `createArchive: ${err.message}`)
-    }
+        let needsTemplateFetch = true
 
-    try {
-      await this.minio.fPutObject(config.assets.packagesBucket, `${parseURI(rootId).uuid}.zip`, archivePath, { 'Content-Type': 'application/zip' })
-      await fs.unlink(archivePath)
-      await fsx.remove(outDir)
+        if (needsTemplateFetch) {
+          await this.minio.fGetObject(config.assets.packagesBucket, 'template.zip', path.join(os.tmpdir(), 'template.zip'))
+        }
+      }
+      catch (err) {
+        api.captureException(err)
+        return send(res, 500, `fetchTemplate: ${err.message}`)
+      }
 
-      const url = await this.minio.presignedGetObject(config.assets.packagesBucket, `${parseURI(rootId).uuid}.zip`)
-      send(res, 200, url)
-    }
-    catch (err) {
-      this.api.captureException(err)
-      return send(res, 500, `uploadArchive: ${err.message}`)
-    }
+      try {
+        const templateEntries = await extractTemplate(path.join(os.tmpdir(), 'template.zip'), outDir)
+
+        archivePath = `${outDir}.zip`
+        const archive = new yazl.ZipFile()
+        await processTemplate(outDir, rootId, templateEntries, mapResults, archive)
+        await storeData(outDir, rootId, mapResults, archive, requestConfig)
+        archive.end()
+
+        await new Promise((resolve, reject) => {
+          archive.outputStream.pipe(fs.createWriteStream(archivePath))
+            .on('error', err => reject(err))
+            .on('close', () => resolve())
+        })
+      }
+      catch (err) {
+        api.captureException(err)
+        return send(res, 500, `createArchive: ${err.message}`)
+      }
+
+      try {
+        await this.minio.fPutObject(config.assets.packagesBucket, `${parseURI(rootId).uuid}.zip`, archivePath, { 'Content-Type': 'application/zip' })
+        await fs.unlink(archivePath)
+        await fsx.remove(outDir)
+
+        const url = await this.minio.presignedGetObject(config.assets.packagesBucket, `${parseURI(rootId).uuid}.zip`)
+        send(res, 200, url)
+      }
+      catch (err) {
+        api.captureException(err)
+        return send(res, 500, `uploadArchive: ${err.message}`)
+      }
+    })
   }
 }
 
